02c42120b325c260063c6f5e0592f9c77f29b85d
Muhammed İlan <ridvanilan@outlook.com>
2022-10-09 16:25:20 +0300

    MusteriManager Düzenlendi

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -12,12 +12,16 @@ namespace ClassMetotDemo
         {
             Console.WriteLine(musteri.FirstName + " " + musteri.LastName + " " + "Eklendi");
             Console.WriteLine("Hesap Bilgiler İçin 1'i Tuşlayınız");
+            Console.WriteLine("Hesap Silmek İçin 2'yi Tuşlayınız");
             var islem = int.Parse(Console.ReadLine());
-            if(islem ==1)
+            if (islem ==1)
             {
-                MusteriListele(musteri);
+               MusteriListele(musteri);
             } 
-
+            else if (islem==2)
+            {
+               MusteriSil(musteri);
+            } 
         }
 
         public void MusteriListele(Musteri musteri)
